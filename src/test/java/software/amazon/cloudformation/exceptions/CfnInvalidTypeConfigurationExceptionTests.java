826f7e7e3175e8ce02a9536ee4527d9540dfaf72
Omkar Hegde <omkhegde@amazon.com>
2021-06-24 11:40:53 -0700

    get changes

--- src/test/java/software/amazon/cloudformation/exceptions/CfnInvalidTypeConfigurationExceptionTests.java
+++ src/test/java/software/amazon/cloudformation/exceptions/CfnInvalidTypeConfigurationExceptionTests.java
@@ -4,12 +4,12 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import org.junit.jupiter.api.Test;
 
 public class CfnInvalidTypeConfigurationExceptionTests {
-    
+
     @Test
     public void cfnInvalidTypeConfigurationException_isBaseHandlerException() {
         assertThatExceptionOfType(BaseHandlerException.class).isThrownBy(() -> {
             throw new CfnInvalidTypeConfigurationException("AWS::Type::Resource", "<request>");
-        }).withCauseInstanceOf(RuntimeException.class).withMessageContaining("<request>")
+        }).withNoCause().withMessageContaining("<request>")
             .withMessageContaining("AWS::Type::Resource")
             .withMessageContaining("Invalid TypeConfiguration");
     }
